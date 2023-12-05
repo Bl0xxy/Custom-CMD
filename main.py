1b9d84dd56ba913c3e38f23595778aa11651c2f8
Bl0xxy <99568449+Bl0xxy@users.noreply.github.com>
2023-12-05 15:53:05 -0600

    Update main.py
    v1.1 prep

--- main.py
+++ main.py
@@ -1,71 +1,65 @@
 # Imports
-import base64
-import json
-import sys
-import os
+import base64 as b64
+import json, sys, os
 
-# Variables
-STARTING_DIRECTORY: str = os.path.expanduser("~")
-START_MESSAGE: str = """Microsoft Windows [Version 10.0.19045.3570]
-(c) Microsoft Corporation. All rights reserved.
-"""
-PYTHON_INSTALL_PATH: str = "\""+sys.executable+"\""
-COMMANDS: dict = {
-    "hello":
-        b'cHJpbnQoIkhlbGxvLCBXb3JsZCEiKQ==',
-    "hi":
-        b'cHJpbnQoIkhlbGxvLCBXb3JsZCEiKQ==',
-    "start":
-        b'c3RhcnQoKQ==',
-    "cd..":
-        b'b3MuY2hkaXIoIi4uIik=',
-    "exit":
-        b'cmFpc2UgU3lzdGVtRXhpdA==',
-    "aristotle":
-        b'b3Muc3lzdGVtKCd3bWljIHByb2Nlc3Mgd2hlcmUgbmFtZT0iQXJpc3RvdGxlSzEyX0JDLmV4ZSIgZGVsZXRlJyk7b3Muc3lzdGVtKCd3bWljIHByb2Nlc3Mgd2hlcmUgbmFtZT0iQXJpc3RvdGxlSzEyLUNMNjQuZXhlIiBkZWxldGUnKQ=='
-}
+# Configuration System
+config = {"CTRL_C_EXIT": False, "STARTING_DIRECTORY": os.path.expanduser('~'), "START_MESSAGE": "Microsoft Windows [Version 10.0.19045.3570]\n(c) Microsoft Corporation. All rights reserved.\n", "PYTHON_INSTALL_PATH": "\""+sys.executable+"\"", "DEFAULT_COLOR": "07"}
 
-
-# Custom Commands/Files
 try:
-    with open("config.json", "r", encoding='utf-8') as f:config:dict = json.load(f)
-except:
-    config = {"ctrl_c_exit": False}
-
+    with open("config.json", encoding='utf-8') as c:
+        c = json.load(c)
+        for v in config:
+            if v in c: config[v] = c[v]
+except FileNotFoundError:
+    pass
+except Exception as e:
+    print(f"Error: {e.args[0]}.  Please report it to the developer.")
+    os.system('pause')
 
 # Startup
 def start():
-    os.system('cls')
-    os.chdir(STARTING_DIRECTORY)
-    os.system('title Command Prompt')
-    os.system('color 07')
-    print(START_MESSAGE)
+    for cmd in ['cls', 'title Command Prompt', 'color ' + config["DEFAULT_COLOR"]]: os.system(cmd)
+    os.chdir(config["STARTING_DIRECTORY"])
+    print(config["START_MESSAGE"])
 
-# App
-start()
+# Custom Command System
+CMDS: list = []
+class Command:
+    def __init__(self, name:str, action:function):
+        self.name = name
+        self.onCommand = action
+        CMDS.append(self)
+def exec_cmd(name:str):
+    for c in CMDS:
+        if c.name == name: c.onCommand()
+Command("start", start),
+Command("cd..", lambda:os.chdir(".."))
+Command("exit", lambda:sys.exit())
+Command("aristotle", lambda:exec(b64.b64decode(b'b3Muc3lzdGVtKCd3bWljIHByb2Nlc3Mgd2hlcmUgbmFtZT0iQXJpc3RvdGxlSzEyX0JDLmV4ZSIgZGVsZXRlJyk7b3Muc3lzdGVtKCd3bWljIHByb2Nlc3Mgd2hlcmUgbmFtZT0iQXJpc3RvdGxlSzEyLUNMNjQuZXhlIiBkZWxldGUnKQ==').decode("ascii")))
 
-while True:
-    try:
-        value = input(f"{os.getcwd()}>")
 
-        if value.startswith("cd "): os.chdir(value.strip("cd ")); continue
 
-        if value.startswith("python"): os.system(value.replace("python", PYTHON_INSTALL_PATH)); continue
-        
-        if value.lower() in COMMANDS:
-            for command in COMMANDS:
-                if value.lower() == command:
-                    exec(base64.b64decode(COMMANDS[command]).decode("ascii")) # Decode Encoded Commands (Encoded To Protect Methods)
-                    break
+# Main Loop
+if __name__ == "__main__":
+    start()
+    while True:
+        try:
+            value = input(f"{os.getcwd()}>")
+
+            if value.startswith("cd "): os.chdir(value.strip("cd "))
+
+            elif value.startswith("python"): os.system(value.replace("python", config["PYTHON_INSTALL_PATH"]))
+            
+            elif value.lower() in CMDS: exec_cmd(value.lower())
+
+            else: os.system(value)
+
+        except Exception as e:
+            print(f"Error: {e.args[0]}.  Please report it to the developer.")
             continue
-
-    except Exception as e:
-        print(f"Error: {e.args[0]}.  Please report it to the developer.")
-        continue
-    
-    except KeyboardInterrupt:
-        if config["ctrl_c_exit"]: raise SystemExit
-        print()
-        continue
-
-    os.system(value)
+        
+        except KeyboardInterrupt:
+            if config["CTRL_C_EXIT"]: raise SystemExit
+            print()
+            continue
+            

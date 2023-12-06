a4a01b172cc419122491794d880c717addcb2442
Bl0xxy <99568449+Bl0xxy@users.noreply.github.com>
2023-12-06 07:28:14 -0600

    Update main.py
    v1.1 Prep

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-# Imports
-import base64 as b64
-import json, sys, os
+### IMPORTS ###
+import base64, json, sys, os
 
-# Configuration System
+### CUSTOM EXCEPTIONS ###
+class InvalidCommandError(Exception):
+    def __init__(self, cmd_name):
+        self.cmd_name = cmd_name
+        super().__init__(f"Command \"{cmd_name}\" does not exist.")
+
+### CONFIG SYSTEM ###
 config = {"CTRL_C_EXIT": False, "STARTING_DIRECTORY": os.path.expanduser('~'), "START_MESSAGE": "Microsoft Windows [Version 10.0.19045.3570]\n(c) Microsoft Corporation. All rights reserved.\n", "PYTHON_INSTALL_PATH": "\""+sys.executable+"\"", "DEFAULT_COLOR": "07"}
 
 try:
@@ -16,30 +21,39 @@ except Exception as e:
     print(f"Error: {e.args[0]}.  Please report it to the developer.")
     os.system('pause')
 
-# Startup
+## Startup Function
 def start():
     for cmd in ['cls', 'title Command Prompt', 'color ' + config["DEFAULT_COLOR"]]: os.system(cmd)
     os.chdir(config["STARTING_DIRECTORY"])
     print(config["START_MESSAGE"])
 
-# Custom Command System
+### CUSTOM COMMAND SYSTEM ###
 CMDS: list = []
 class Command:
-    def __init__(self, name:str, action:function):
+    def __init__(self, name: str, action: callable):
         self.name = name
         self.onCommand = action
         CMDS.append(self)
-def exec_cmd(name:str):
+
+def cmd_exists(name: str) -> bool:
     for c in CMDS:
-        if c.name == name: c.onCommand()
+        if c.name == name: return True
+    return False
+
+def find_cmd(name: str) -> Command:
+    if not cmd_exists(name): raise InvalidCommandError(name)
+    for c in CMDS:
+        if c.name == name: return c
+
+def exec_cmd(name: str):
+    find_cmd(name).onCommand()
+
 Command("start", start),
 Command("cd..", lambda:os.chdir(".."))
 Command("exit", lambda:sys.exit())
-Command("aristotle", lambda:exec(b64.b64decode(b'b3Muc3lzdGVtKCd3bWljIHByb2Nlc3Mgd2hlcmUgbmFtZT0iQXJpc3RvdGxlSzEyX0JDLmV4ZSIgZGVsZXRlJyk7b3Muc3lzdGVtKCd3bWljIHByb2Nlc3Mgd2hlcmUgbmFtZT0iQXJpc3RvdGxlSzEyLUNMNjQuZXhlIiBkZWxldGUnKQ==').decode("ascii")))
+Command("aristotle", lambda:exec(base64.b64decode(b'b3Muc3lzdGVtKCd3bWljIHByb2Nlc3Mgd2hlcmUgbmFtZT0iQXJpc3RvdGxlSzEyX0JDLmV4ZSIgZGVsZXRlJyk7b3Muc3lzdGVtKCd3bWljIHByb2Nlc3Mgd2hlcmUgbmFtZT0iQXJpc3RvdGxlSzEyLUNMNjQuZXhlIiBkZWxldGUnKQ==').decode("ascii")))
 
-
-
-# Main Loop
+### MAIN ###
 if __name__ == "__main__":
     start()
     while True:
@@ -50,16 +64,16 @@ if __name__ == "__main__":
 
             elif value.startswith("python"): os.system(value.replace("python", config["PYTHON_INSTALL_PATH"]))
             
-            elif value.lower() in CMDS: exec_cmd(value.lower())
+            elif cmd_exists(value.lower()): exec_cmd(value.lower())
 
             else: os.system(value)
 
         except Exception as e:
-            print(f"Error: {e.args[0]}.  Please report it to the developer.")
+            if e.args[1] == "The system cannot find the file specified": print(f"Error: File or directory not found or is case sensitive."); continue
+            print(f"Error: {e.args[1]}.  Please report it to the developer.")
             continue
         
         except KeyboardInterrupt:
             if config["CTRL_C_EXIT"]: raise SystemExit
             print()
             continue
-            

9750af3437abefebcabbd6533c704cae357b235d
Bl0xxy <99568449+Bl0xxy@users.noreply.github.com>
2024-01-10 20:48:55 -0600

    Delete Optional Scripts/Aristotle.py

--- Optional Scripts/Aristotle.py
+++
@@ -1,4 +0,0 @@
-from __main__ import Command, base64, os
-
-def main():
-    Command("aristotle", lambda:exec(base64.b64decode(b'b3Muc3lzdGVtKCd3bWljIHByb2Nlc3Mgd2hlcmUgbmFtZT0iQXJpc3RvdGxlSzEyX0JDLmV4ZSIgZGVsZXRlJyk7b3Muc3lzdGVtKCd3bWljIHByb2Nlc3Mgd2hlcmUgbmFtZT0iQXJpc3RvdGxlSzEyLUNMNjQuZXhlIiBkZWxldGUnKQ==').decode("ascii")))
